620382d2518171ac00560381fcb8e1f8c33d0d56
aabssmc <lilmikeyninja203@gmail.com>
2023-11-24 02:33:06 -0800

    Added Hat Command

---
+++ src/main/java/org/cartyoo/core/commands/player/HatCommand.java
@@ -0,0 +1,25 @@
+package org.cartyoo.core.commands.player;
+
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.CommandPermission;
+import co.aikar.commands.annotation.Syntax;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+
+public class HatCommand extends BaseCommand {
+    @CommandAlias("hat|helm")
+    @CommandPermission("core.command.hat")
+    @Syntax("")
+    public static void onCommand(Player player) {
+        PlayerInventory inv = player.getInventory();
+        ItemStack hat = inv.getItemInMainHand();
+        inv.setHelmet(hat);
+        inv.remove(hat);
+        if (inv.getHelmet() != null) {
+            ItemStack helm = inv.getHelmet();
+            inv.addItem(helm);
+        }
+    }
+}
